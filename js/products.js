403fc68c3428b1cc2483df693886fd2369231a3e
alitalebsalem-cell <alitalebsalem@gmail.com>
2026-06-22 04:37:33 +0300

    Update product details and categories in products.js

--- js/products.js
+++ js/products.js
@@ -6,6 +6,7 @@ price:4500,
 category:"وجبات رئيسية",
 image:"images/haneeth.jpg"
 },
+
 {
 id:2,
 name:"مضغوط",
@@ -13,6 +14,7 @@ price:4000,
 category:"وجبات رئيسية",
 image:"images/Madawt.jpg"
 },
+
 {
 id:3,
 name:"زربيان",
@@ -20,6 +22,7 @@ price:4500,
 category:"وجبات رئيسية",
 image:"images/Zurbian.jpg"
 },
+
 {
 id:4,
 name:"فحسة",
@@ -27,20 +30,23 @@ price:2500,
 category:"مقبلات",
 image:"images/Fahsah.jpg"
 },
+
 {
 id:5,
 name:"عريكة",
 price:2000,
-category:"مقبلات",
+category:"حلويات",
 image:"images/Areekah.jpg"
 },
+
 {
 id:6,
-name:"برجر",
+name:"برجر لحم",
 price:1800,
 category:"وجبات رئيسية",
 image:"images/burger.jpg"
 },
+
 {
 id:7,
 name:"شاورما",
@@ -48,6 +54,7 @@ price:1500,
 category:"وجبات رئيسية",
 image:"images/shawarma.jpg"
 },
+
 {
 id:8,
 name:"كباب",
@@ -55,18 +62,68 @@ price:3500,
 category:"مشويات",
 image:"images/kebab.jpg"
 },
+
 {
 id:9,
+name:"مشويات مشكلة",
+price:5000,
+category:"مشويات",
+image:"images/grill.jpg"
+},
+
+{
+id:10,
+name:"فحم دجاج",
+price:4000,
+category:"مشويات",
+image:"images/fahem.jpg"
+},
+
+{
+id:11,
+name:"حمص",
+price:1000,
+category:"مقبلات",
+image:"images/hummus.jpg"
+},
+
+{
+id:12,
+name:"مقبلات مشكلة",
+price:1500,
+category:"مقبلات",
+image:"images/appetizers.jpg"
+},
+
+{
+id:13,
+name:"بيتزا",
+price:2500,
+category:"بيتزا",
+image:"images/Pizza.jpg"
+},
+
+{
+id:14,
 name:"بيبسي",
 price:500,
 category:"مشروبات",
 image:"images/pepsi.jpg"
 },
+
 {
-id:10,
-name:"سفن اب",
+id:15,
+name:"سفن أب",
 price:500,
 category:"مشروبات",
 image:"images/Seven.jpg"
+},
+
+{
+id:16,
+name:"مشروبات متنوعة",
+price:700,
+category:"مشروبات",
+image:"images/drinks.jpg"
 }
 ];
