2304a8611c01de7231cae991c88ee110886ddae0
alitalebsalem-cell <alitalebsalem@gmail.com>
2026-06-22 04:11:41 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,72 @@
-
+const products = [
+{
+id:1,
+name:"حنيذ",
+price:4500,
+category:"وجبات رئيسية",
+image:"images/haneeth.jpg"
+},
+{
+id:2,
+name:"مضغوط",
+price:4000,
+category:"وجبات رئيسية",
+image:"images/Madawt.jpg"
+},
+{
+id:3,
+name:"زربيان",
+price:4500,
+category:"وجبات رئيسية",
+image:"images/Zurbian.jpg"
+},
+{
+id:4,
+name:"فحسة",
+price:2500,
+category:"مقبلات",
+image:"images/Fahsah.jpg"
+},
+{
+id:5,
+name:"عريكة",
+price:2000,
+category:"مقبلات",
+image:"images/Areekah.jpg"
+},
+{
+id:6,
+name:"برجر",
+price:1800,
+category:"وجبات رئيسية",
+image:"images/burger.jpg"
+},
+{
+id:7,
+name:"شاورما",
+price:1500,
+category:"وجبات رئيسية",
+image:"images/shawarma.jpg"
+},
+{
+id:8,
+name:"كباب",
+price:3500,
+category:"مشويات",
+image:"images/kebab.jpg"
+},
+{
+id:9,
+name:"بيبسي",
+price:500,
+category:"مشروبات",
+image:"images/pepsi.jpg"
+},
+{
+id:10,
+name:"سفن اب",
+price:500,
+category:"مشروبات",
+image:"images/Seven.jpg"
+}
+];
